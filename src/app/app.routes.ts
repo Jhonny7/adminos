18de84aae267d9febb9f41a6969d165fd3a8c899
10034189 <cesar.paniagua@elektra.com.mx>
2026-04-24 18:32:11 -0600

    add predios and theme

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Home } from './pages/Home/home.component';
 import { Dashboard } from './pages/Dashboard/dashboard.component';
 import { Labors } from './pages/Labors/labors.component';
 import { Plaguicidas } from './pages/Plaguicidas/plaguicidas.component';
+import { Predios } from './pages/Predios/predios.component';
 import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
@@ -26,6 +27,7 @@ export const routes: Routes = [
       { path: 'resumen', component: Dashboard },
       { path: 'labores', component: Labors },
       { path: 'plaguicidas', component: Plaguicidas },
+      { path: 'predios', component: Predios },
       { path: 'home', component: Home }
     ]
   }
